943d56cffeb0846b8cb4edd6b96a852ec8d24d3f
Paulo Victor de Oliveira Leal <p_leal@ymail.com>
2017-08-31 19:08:34 -0300

    Inclusao do projeto

---
+++ JoySpeech/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Speech.Recognition;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace JoySpeech {
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent( );
+            // Inicializando a engine de reconhecimento
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine( new System.Globalization.CultureInfo( "pt-BR" ) );
+
+            sre.SetInputToDefaultAudioDevice( );
+
+            // Criando a gramatica
+            Choices colors = new Choices( );
+            colors.Add( new string[ ] { "vermelho", "verde", "azul", "preto", "açogueiro", "cima", "baixo", "paralelepipedo", "mover para frente" } );
+
+            GrammarBuilder gb = new GrammarBuilder( );
+            gb.Append( colors );
+
+            Grammar g = new Grammar( gb );
+
+            // Adicionando a gramatica na engine
+            sre.LoadGrammar( g );
+            
+            Console.WriteLine( ">> Valendo" );
+            while (true) {
+                sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>( sre_SpeechRecognized );
+
+                sre.Recognize( );
+            }
+        }
+
+        static void sre_SpeechRecognized( object sender, SpeechRecognizedEventArgs e ) {
+            Console.WriteLine( "> RECONHECIDO: " + e.Result.Text );
+            Vocals.VirtualKeyboard.PressKey( Keys.J, Keys.J );
+        }
+    }
+}
---
+++ JoySpeech/VirtualKeyboard.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vocals {
+    public static class VirtualKeyboard {
+        [StructLayout( LayoutKind.Sequential )]
+        internal struct KEYBOARDINPUT {
+            public uint type;
+            public ushort vk;
+            public ushort scanCode;
+            public uint flags;
+            public uint time;
+            public uint extrainfo;
+            public uint padding1;
+            public uint padding2;
+        }
+
+        [StructLayout( LayoutKind.Sequential )]
+        internal struct MOUSEINPUT {
+            public uint dx;
+            public uint dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
+        [StructLayout( LayoutKind.Sequential )]
+        internal struct HARDWAREINPUT {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+
+        [StructLayout( LayoutKind.Explicit )]
+        internal struct INPUT {
+            [FieldOffset( 0 )]
+            public int type;
+            [FieldOffset( 4 )]
+            public MOUSEINPUT mi;
+            [FieldOffset( 4 )]
+            public KEYBOARDINPUT ki;
+            [FieldOffset( 4 )]
+            public HARDWAREINPUT hi;
+        }
+
+        [DllImport( "User32.dll" )]
+        private static extern uint SendInput( uint numberOfInputs, ref INPUT input,
+        int structSize );
+
+
+        [Flags]
+        public enum KeyFlag {
+            KeyDown = 0x0000,
+            KeyUp = 0x0002,
+            Scancode = 0x0008
+        }
+
+        public static void SendKey( uint keyCode, KeyFlag keyFlag ) {
+            INPUT InputData = new INPUT( );
+
+            InputData.type = 1;
+            InputData.ki.scanCode = ( ushort ) keyCode;
+            InputData.ki.flags = ( uint ) keyFlag;
+            Console.WriteLine( InputData.ki.scanCode );
+            Console.WriteLine( keyCode );
+
+            SendInput( ( uint ) 1, ref InputData, ( int ) Marshal.SizeOf( typeof( INPUT ) ) );
+        }
+
+        [DllImport( "User32.dll" )]
+        private static extern uint MapVirtualKey( uint uCode, uint uMapType );
+
+        public static void PressKey( Keys key, Keys modifier ) {
+
+            uint keyCodeModifier = ( uint ) modifier;
+            uint scanCodeModifier = MapVirtualKey( keyCodeModifier, 0 );
+            VirtualKeyboard.SendKey( scanCodeModifier, KeyFlag.KeyDown | KeyFlag.Scancode );
+
+            uint keyCode = ( uint ) key;
+            uint scanCode = MapVirtualKey( keyCode, 0 );
+            VirtualKeyboard.SendKey( scanCode, KeyFlag.KeyDown | KeyFlag.Scancode );
+            System.Threading.Thread.Sleep( ( int ) ( 100 ) );
+            VirtualKeyboard.SendKey( scanCode, KeyFlag.KeyUp | KeyFlag.Scancode );
+            VirtualKeyboard.SendKey( scanCodeModifier, KeyFlag.KeyUp | KeyFlag.Scancode );
+
+
+        }
+
+    }
+}
